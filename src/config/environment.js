3a480f3ac8379efae340f4656980560c5b1c8990
Martin <martin.gamboa@cencosud.com>
2023-12-10 13:17:11 -0300

    LP-08: Node_modules added to gitignore, env-example created

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,5 @@
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 export const CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const API_KEY = process.env.CLOUDINARY_API_KEY;
 export const API_SECRET = process.env.CLOUDINARY_API_SECRET;
